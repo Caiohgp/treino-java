9253d32dadd9cc147f6d5dba1be43620b6dae14e
Caiohgp <caio.102008@gmail.com>
2021-09-28 09:16:12 -0300

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+import caiohgp.treinojava.classes.dominio.Carro;
+
+public class Test {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Fiat Uno";
+        carro1.modelo = "ELX";
+        carro1.ano = 1997;
+
+        carro2.nome = "Opala";
+        carro2.modelo = "Diplomata";
+        carro2.ano = 1990;
+
+        System.out.println(carro1.nome + " " + carro1.modelo + " " + carro1.ano);
+        System.out.println(carro2.nome + " " + carro2.modelo + " " + carro2.ano);
+
+    }
+}
