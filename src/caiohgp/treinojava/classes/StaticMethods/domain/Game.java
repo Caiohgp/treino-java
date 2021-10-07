9e30b4ed50e0dad063ae048207e41173799695a2
Caiohgp <caio.102008@gmail.com>
2021-10-07 16:20:23 -0300

    Added Static Method Training

---
+++ src/caiohgp/treinojava/classes/StaticMethods/domain/Game.java
@@ -0,0 +1,63 @@
+package caiohgp.treinojava.classes.StaticMethods.domain;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Game {
+    private String[] teams;
+    private String league;
+    private String date;
+    private static int players;
+
+    public Game(String[] teams, String league, String date){
+        this.teams = teams;
+        this.league = league;
+        this.date = date;
+    }
+
+    public Game(){
+    }
+
+    public void print(){
+        System.out.println("------- Teams -------");
+        for(String teams : this.teams){
+            System.out.println(teams);
+        }
+        System.out.println("---------------------");
+        System.out.println("League - " + this.league);
+        System.out.println("Date and time - " + this.date);
+
+    }
+
+    public String[] getTeams() {
+        return teams;
+    }
+
+    public void setTeams(String[] teams) {
+        this.teams = teams;
+    }
+
+    public String getLeague() {
+        return league;
+    }
+
+    public void setLeague(String league) {
+        this.league = league;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public static int getPlayers() {
+        return players;
+    }
+
+    public static void setPlayers(int players) {
+        Game.players = players;
+    }
+}
---
+++ src/caiohgp/treinojava/classes/StaticMethods/test/GameTest.java
@@ -0,0 +1,17 @@
+package caiohgp.treinojava.classes.StaticMethods.test;
+
+import caiohgp.treinojava.classes.StaticMethods.domain.Game;
+
+import java.time.LocalDate;
+
+public class GameTest {
+    public static void main(String[] args){
+        String[] teams = {"Corinthians","Palmeiras"};
+        String date = String.valueOf(LocalDate.now());
+        Game game1 = new Game(teams, "Brasileirão", date);
+
+
+        game1.setPlayers(22);
+        game1.print();
+    }
+}
